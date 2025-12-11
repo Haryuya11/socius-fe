bd65ff2db0b864866bcef5c3e4745a4c260eb48b
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2025-12-11 16:22:14 +0700

    feat: add profile menu

---
+++ utils/name-utils.ts
@@ -0,0 +1,10 @@
+export const getFullName = (first_name: string, last_name: string) => {
+  if (!first_name || !last_name) return "??";
+
+  return first_name + " " + last_name;
+};
+
+export const getInitials = (first_name: string, last_name: string) => {
+  if (!first_name || !last_name) return "??";
+  return `${first_name.charAt(0)}${last_name.charAt(0)}`.toUpperCase();
+};
